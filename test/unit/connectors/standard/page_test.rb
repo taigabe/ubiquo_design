ad6f025c69ee76732e1485d89d7ee7eb9d9e9b4b
Albert Callarisa <shark234@gmail.com>
2009-07-10 15:36:41 +0200

    removed unused test

--- test/unit/connectors/standard/page_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + "/../../../../../../../test/test_helper.rb"
-
-module Connectors
-  module Standard
-    class PageTest < ActiveSupport::TestCase
-      
-    end
-  end
-end
