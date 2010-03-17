3b8e234b448003f9b38322be743468278b0ba041
Toni Reina <areina0@gmail.com>
2010-03-17 09:16:34 +0100

    Removed fixtures and moved its content to example migration

---
+++ install/db/migrate/20100316164303_create_example_design_structure.rb
@@ -0,0 +1,45 @@
+class CreateExampleDesignStructure < ActiveRecord::Migration
+  def self.up
+    pt_simple = PageTemplate.create(:name => "Simple",
+                                    :key => "simple",
+                                    :layout => "main",
+                                    :thumbnail_file_name => "simple.png")
+    pt_interior = PageTemplate.create(:name => "Interior",
+                                      :key => "interior",
+                                      :layout => "main",
+                                      :thumbnail_file_name => "interior.png")
+    bt_top = BlockType.create(:name => "Top block",
+                              :key => "top",
+                              :can_use_default_block => true)
+    bt_sidebar = BlockType.create(:name => "Sidebar block",
+                              :key => "sidebar",
+                              :can_use_default_block => true)
+    bt_main = BlockType.create(:name => "Main block",
+                              :key => "main",
+                              :can_use_default_block => false)
+    
+    ct_free = ComponentType.create(:name => "Free component",
+                                   :key => "free",
+                                   :is_configurable => true,
+                                   :subclass_type => "Free")
+    ct_assets_automatic_menu = ComponentType.create(:name => "Menu automàtic per recursos",
+                                                    :key => "assets_automatic_menu",
+                                                    :is_configurable => true,
+                                                    :subclass_type => "AssetsAutomaticMenu")
+    
+    # relate page templates with block types
+    
+    pt_simple.block_types << [bt_top, bt_sidebar, bt_main]
+    pt_interior.block_types << [bt_top, bt_sidebar, bt_main]
+    
+    # relate page templates with component types
+    
+    pt_simple.component_types << [ct_free, ct_assets_automatic_menu]
+    pt_interior.component_types << [ct_free, ct_assets_automatic_menu]
+  end
+
+  def self.down
+    PageTemplate.find_by_key("simple").destroy
+    PageTemplate.find_by_key("interior").destroy    
+  end
+end
