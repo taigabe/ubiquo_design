5042f5b9acc1c823c4c8dd2e82ab387a6343e650
Bernat Foj Capell <bfoj@gnuine.com>
2010-07-02 10:33:54 +0200

    Changed widget code paths to a more rails-like schema

--- generators/ubiquo_widget/ubiquo_widget_generator.rb
+++ generators/ubiquo_widget/ubiquo_widget_generator.rb
@@ -7,17 +7,19 @@ class UbiquoWidgetGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
       break if @name.blank?
-      m.directory('app/models/')
-      m.directory(File.join('app/widgets/', @name, 'views', 'ubiquo'))
+      m.directory('app/models/widgets')
+      m.directory(File.join('app/widgets'))
+      m.directory(File.join('app/views/widgets/', @name, 'views', 'ubiquo'))
+      m.directory(File.join('test/unit/widgets'))
       m.directory(File.join('test/functional/widgets'))
       m.directory(File.join('test/functional/widgets/ubiquo'))
       
-      m.template('widget.rb.erb', File.join('app/widgets', @name, "widget.rb"))
-      m.template('views/_show.html.erb', File.join('app/widgets', @name, "views", "_show.html.erb"))
-      m.template('views/ubiquo/_form.html.erb', File.join('app/widgets', @name, "views", "ubiquo", "_form.html.erb"))
-      m.template('models/component.rb.erb', File.join('app/models', "#{@name}.rb"))
+      m.template('widget.rb.erb', File.join('app/widgets', "#{@name}_widget.rb"))
+      m.template('views/_show.html.erb', File.join('app/views/widgets', @name, "_show.html.erb"))
+      m.template('views/ubiquo/_form.html.erb', File.join('app/views/widgets', @name, "views", "ubiquo", "_form.html.erb"))
+      m.template('models/component.rb.erb', File.join('app/models/widgets', "#{@name}.rb"))
 
-      m.template('test/unit/widget_test.rb.erb', File.join('test/unit', "#{@name}_test.rb"))
+      m.template('test/unit/widget_test.rb.erb', File.join('test/unit/widgets', "#{@name}_test.rb"))
       m.template('test/functional/widget_test.rb.erb', File.join('test/functional/widgets', "#{@name}_test.rb"))
       m.template('test/functional/ubiquo/widget_test.rb.erb', File.join('test/functional/widgets/ubiquo', "#{@name}_test.rb"))
       m.migration_template 'migration.rb', 'db/migrate', :assigns => { 
