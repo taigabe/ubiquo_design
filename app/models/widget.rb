bcf960304eaf68ceca59e237297ef9522f380262
Bernat Foj Capell <bfoj@gnuine.com>
2010-07-17 13:33:50 +0200

    Updated generated unit test for widgets

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,15 +1,32 @@
 class Widget < ActiveRecord::Base
 
+  INACCEPTABLE_OPTIONS = %w{options widget widget_id block block_id position}
+
   @@behaviours = {}
 
   cattr_accessor :behaviours
 
   validates_presence_of :name, :key, :subclass_type
   validates_uniqueness_of :key
-  has_many :components, :order => 'components.position ASC'
+
+  belongs_to :block
 
   def self.behaviour(name, options={}, &block)
     @@behaviours[name] = {:options => options, :block => block}
   end
+
+  # cattr_accessor :allowed_options_storage
+  def self.allowed_options=(opts)
+    opts = [opts].flatten
+    unallowed_options = opts.map(&:to_s)&INACCEPTABLE_OPTIONS
+    raise "Inacceptable options: '%s'" % unallowed_options.join(', ') unless unallowed_options.blank?
+    self.cattr_accessor :allowed_options_storage
+    self.allowed_options_storage = opts
+  end
+
+  def self.allowed_options
+    self.allowed_options_storage ||= []
+  end
+
 
 end
