ae856d63b678595fdd1e10c482e18c63ff517d9f
Bernat Foj Capell <bfoj@gnuine.com>
2010-10-19 12:39:06 +0200

    Fixed StaticSection unit tests

--- install/test/unit/widgets/static_section_test.rb
+++ install/test/unit/widgets/static_section_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
 class StaticSectionTest < ActiveSupport::TestCase
-  
+
   def test_should_create_static_section
     assert_difference 'StaticSection.count' do
       static_section = create_static_section
@@ -9,11 +9,19 @@ class StaticSectionTest < ActiveSupport::TestCase
     end
   end
 
+  def test_should_require_title
+    assert_no_difference 'StaticSection.count' do
+      static_section = create_static_section(:title => '')
+      assert static_section.errors.on(:title)
+    end
+  end
+
   private
-  
+
   def create_static_section(options = {})
     default_options = {
-      :name => "Test static_section", 
+      :name => "Test static_section",
+      :title => 'Static section title',
       :block => blocks(:one),
       # Insert other options for widget here
     }
