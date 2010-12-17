90b94ffa10c6ffd505c1337e6d78333d6bfc7b09
Bernat Foj Capell <bfoj@gnuine.com>
2010-12-17 20:57:10 +0100

    Connectors code refactoring to use the core Connector::Base [#295]

--- lib/ubiquo_design/connectors/base.rb
+++ lib/ubiquo_design/connectors/base.rb
@@ -1,6 +1,6 @@
 module UbiquoDesign
   module Connectors
-    class Base
+    class Base < Ubiquo::Connectors::Base
 
       # loads this connector. It's called if that connector is used
       def self.load!
@@ -22,42 +22,6 @@ module UbiquoDesign
         UbiquoDesign::Connectors::Base.set_current_connector self
       end
 
-      # Register the uhooks methods in connectors to be used in klass
-      def self.register_uhooks klass, *connectors
-        connectors.each do |connector|
-          connector.instance_methods.each do |method|
-            if method =~ /^uhook_(.*)$/
-              connectorized_method = "uhook_#{self.to_s.demodulize.underscore}_#{$~[1]}"
-              connector.send :alias_method, connectorized_method, method
-              if klass.instance_methods.include?(method)
-                klass.send :alias_method, method, connectorized_method
-              else
-                class << klass
-                  self
-                end.send :alias_method, method, connectorized_method
-              end
-              connector.send :undef_method, connectorized_method
-            end
-          end
-        end
-      end
-
-      def self.current_connector
-        @current_connector
-      end
-
-      def self.set_current_connector klass
-        @current_connector = klass
-      end
-
-      # Possible cleanups to perform
-      def self.unload!; end
-
-      # Validate here the possible connector requirements and dependencies
-      def self.validate_requirements; end
     end
-
-    # Raised when a connector requirement is not met
-    class ConnectorRequirementError < StandardError; end
   end
 end
