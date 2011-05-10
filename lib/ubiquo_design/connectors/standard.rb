2f253d2fda7d9c5a5dd748f4257cb655d95d0198
Toni Reina <areina0@gmail.com>
2011-05-10 16:39:22 +0200

    Added classes for edit and remove links [#438]

--- lib/ubiquo_design/connectors/standard.rb
+++ lib/ubiquo_design/connectors/standard.rb
@@ -167,9 +167,9 @@ module UbiquoDesign
         module Helper
           def uhook_page_actions(page)
             [
-              link_to(t('ubiquo.edit'), edit_ubiquo_page_path(page)),
+              link_to(t('ubiquo.edit'), edit_ubiquo_page_path(page), :class => 'btn-edit'),
               link_to(t('ubiquo.design.design'), ubiquo_page_design_path(page)),
-              (link_to(t('ubiquo.remove'), [:ubiquo, page], :confirm => t('ubiquo.design.confirm_page_removal'), :method => :delete) unless page.key?)
+              (link_to(t('ubiquo.remove'), [:ubiquo, page], :confirm => t('ubiquo.design.confirm_page_removal'), :method => :delete, :class => 'btn-delete') unless page.key?)
             ].compact
           end
 
