368e87ec9aa3a0999a41d1c7a1beb7587e93c5cf
Bernat Foj Capell <bfoj@gnuine.com>
2011-07-06 10:37:20 +0200

    Added first Varnish TTL test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < PublicController
 
   # Renders a Page using its associated template, displaying its blocks and widgets
   def show
+    response.headers['X-VARNISH-TTL'] = '120'
     @page = uhook_load_page
     render_page @page
   end
