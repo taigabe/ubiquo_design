d579570d18dbbc876de144352c440fc408c75650
David Lozano <dlozano@gnuine.com>
2012-05-09 10:56:26 +0200

    Refactored widget update_page method

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -184,8 +184,13 @@ class Widget < ActiveRecord::Base
   # When a block is saved, the associated page must change its modified attribute
   def update_page
     if self.update_page_denied.blank?
-      widget_page = self.block.reload.page.reload
-      widget_page.update_modified(true) unless widget_page.is_modified?
+      if block = self.block
+        block.reload
+        if widget_page = block.page
+          widget_page.reload
+          widget_page.update_modified(true) unless widget_page.is_modified?
+        end
+      end
     end
   end
 
